2b091b78fde76ce3f5f421e983ef9712bd80e3a8
kiplangat meshack <meshnobie@gmail.com>
2023-04-12 01:52:13 +0300

    ft:styled card display

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -5,18 +5,21 @@ const Bloglist = (props) => {
     const blogs = props.blogs
     const handleDelete = props.handleDelete
     return ( 
-        <div className="blog-list">
+        <div className="blog-list container">
              {blogs.map((blog) => (
-                <div className="blog-preview card"  key={blog.id}>
-                    <img src= { blog.image } alt="" />
+               <div className="row">
+                 <div className="col-sm-4">
+                    <div className="blog-preview card column"  key={blog.id}>
+                    <img className="cardimg"  src= { blog.image } alt="" />
                     <div class="card-body">
-                       
                     <h2 class="card-title">{ blog.title }</h2>
                     <h6 class="card-text">{ blog.description}</h6>
                     <p>writen by: { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete</button>
+                    <button onClick={() => handleDelete(blog.id)}> delete </button>
                     </div>
                 </div>
+                </div>
+               </div>
             ))}
         </div>
      );
