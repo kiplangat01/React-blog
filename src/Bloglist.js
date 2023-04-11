fa0d8fba2809c09395bd4062c797552bd8977e11
kiplangat meshack <meshnobie@gmail.com>
2023-04-11 23:01:14 +0300

    ft:fetched  apis

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -6,6 +6,7 @@ const Bloglist = (props) => {
              {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
+                    <h6>{ blog.description}</h6>
                     <p>writen by: { blog.author }</p>
                     <button onClick={() => handleDelete(blog.id)}>delete</button>
                 </div>
--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,24 @@ import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "title1", body: "react is owesome to study", author: "mesh", id: 1},
-        { title: "title2", body: "this is my first time learning react", author: "peter", id: 2},
-        { title: "title3", body: "i am enjoying the progress", author: "john", id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
     }
     useEffect(() => {
-        console.log('d')
+       fetch('http://localhost:8000/blogs')
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            setBlogs(data)
+          })
     })
     return ( 
         <div className="home">
-           <Bloglist blogs={blogs} handleDelete={  handleDelete }/>
+           {blogs && <Bloglist blogs={blogs} handleDelet e={  handleDelete } />}
         </div>
      );
 }
