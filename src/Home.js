a5733ab75a9f7850ec7d092f86d2e68390a4bb7b
kiplangat meshack <meshnobie@gmail.com>
2023-04-11 17:35:24 +0300

    ft:outputted blogs in home component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "title1", body: "react is owesome to study", author: "mesh", id: 1},
+        { title: "title2", body: "this is my first time learning react", author: "peter", id: 2},
+        { title: "title1", body: "i am enjoying the progress", author: "john", id: 3}
+    ])
     return ( 
-        <h2>home page compo</h2>
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <h5>{blog.body}</h5>
+                    <p>writen by: { blog.author }</p>
+                </div>
+            ))}
+        </div>
      );
 }
  
