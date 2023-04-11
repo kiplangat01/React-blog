dce8ba8f24f0715a7320003b1541c479996a44ae
kiplangat meshack <meshnobie@gmail.com>
2023-04-11 17:21:48 +0300

    ft:added multiple components

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        <h2>home page compo</h2>
+     );
+}
+ 
+export default Home;
---
+++ src/Navabar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>my blogs</h1>
+            <a href="/">home</a>
+            <a href="/create">create</a>
+        </div>
+     );
+}
+ 
+export default Navbar;
