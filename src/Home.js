07eecbe7549c6feb30a20204cdd6b3afecaccea0
kiplangat meshack <meshnobie@gmail.com>
2023-04-12 20:38:39 +0300

    ft:displayed message why fetching data

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -15,11 +16,13 @@ const Home = () => {
             return res.json();
           })
           .then(data => {
-            setBlogs(data)
+            setBlogs(data);
+            setIsPending(false);
           })
     })
     return ( 
         <div className="home">
+          { isPending && <div><h5>loading...</h5></div>}
             <section><h1>Motivation </h1></section>
            {blogs && <Bloglist blogs={blogs} handleDelet e={  handleDelete } />}
         </div>
