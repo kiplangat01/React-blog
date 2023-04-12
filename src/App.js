e77fc7124b4cc5d11331716e5983c5e85f991a26
kiplangat meshack <meshnobie@gmail.com>
2023-04-12 22:54:36 +0300

    ft:added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import './index.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Navbar from './Navabar';
 
+
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
+      <Switch>
+        <Route path='/'>
       <Home />
+      </Route>
+      </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
