b478d5a4094df7e12c60d0aed5afb478ecc37d1c
kiplangat meshack <meshnobie@gmail.com>
2023-04-12 22:11:16 +0300

    ft:created a custom hook

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Bloglist = (props) => {
     const blogs = props.blogs
-    const handleDelete = props.handleDelete
+   
     return ( 
         <div className="blog-list container">
           <section><h1>Motivation </h1></section>
@@ -13,10 +13,10 @@ const Bloglist = (props) => {
                     <div className="blog-preview card column"  key={blog.id}>
                     <img className="cardimg"  src= { blog.image } alt="" />
                     <div class="card-body">
-                    <h2 class="card-title">{ blog.title }</h2>
-                    <h6 class="card-text">{ blog.description}</h6>
+                    <h4 class="card-title">{ blog.title }</h4>
+                    <hp class="card-text">{ blog.description}</hp>
                     <p>writen by: { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}> delete </button>
+                
                     </div>
                 </div>
                 </div>
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,18 @@
-import { useState, useEffect } from "react";
 import './index.css';
 import Bloglist from "./Bloglist";
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true)
-    const[error, setError] = useState(null);
+   const{ data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
-    useEffect(() => {
-       fetch('http://localhost:8000/blogs')
-          .then(res => {
-            if (!res.ok){
-              throw Error('resource not found')
-            }
-            return res.json();
-          })
-          .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null)
-          })
-          .catch(err => {
-            setIsPending(false)
-            setError(err.message);
-          })
-    })
+
+   
+    
     return ( 
         <div className="home">
           { error && <div><h5>{ error }</h5></div>}
           { isPending && <div><h5>wait a minute</h5></div>} 
-           {blogs && <Bloglist blogs={blogs} handleDelet e={  handleDelete } />}
+           {blogs && <Bloglist blogs={blogs}   />}
         </div>
      );
 }
