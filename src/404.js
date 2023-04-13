9e87b360d1878f91d8d3a7eb27160c64ac96de28
kiplangat meshack <meshnobie@gmail.com>
2023-04-13 22:21:24 +0300

    ft:created 404 pages

---
+++ src/404.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return ( 
+        <div className="notfound">
+            <h1>sorry could find that page </h1>
+            <a href="/">home</a>
+        </div>
+     );
+}
+ 
+export default NotFound;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Navbar from './Navabar';
 import Post from './Post';
 import PostDetails from './PostDetail';
+import NotFound from './404';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
       </Route>
       <Route path='/blogs/:id'>
         <PostDetails />
+      </Route>
+      <Route path='*'>
+        <NotFound />
       </Route>
       </Switch>
       </div>
