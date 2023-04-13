70fe5e93cc3fc6a2e2fdb0c711054dd79fb48c9e
kiplangat meshack <meshnobie@gmail.com>
2023-04-13 14:05:07 +0300

    ft:enabled post request for blogs and displayed home

--- src/Post.js
+++ src/Post.js
@@ -1,27 +1,60 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
-const Post = () => {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [description, setDecription] = useState('')
-    const [body, setBody] = useState('')
-    return ( 
-       <div className="create">
-        <h2>Post a new blog</h2>
-        <form action="">
-            <label htmlFor="title">title</label>
-            <input type="text" required value={ title } onChange={(e) => setTitle(e.target.value)} />
-            <label htmlFor="">Description</label>
-            <textarea name="" required onChange={(e) => setDecription(e.target.value)}></textarea>
-            <label htmlFor="">Post details</label>
-            <textarea name="" required onChange={(e) => setBody(e.target.value)}></textarea>
-            <label htmlFor="" >Author</label>
-            <input type="text" value={ author } onChange={(e) => setAuthor(e.target.value)} />
-            <button>Post</button>
-            
-        </form>
-       </div>
-     );
+function PostForm() {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState(null);
+  const [body, setBody] = useState('');
+  const [description, setDescription] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const blog = {title, image, body, description, author};
+
+    fetch('http://localhost:8000/blogs',{
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blog)
+    }).then(() => {
+        console.log('new blog added')
+    })
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Image:
+        <input type="file" onChange={handleImageChange} />
+      </label>
+      <br />
+      <label>
+        Body:
+        <textarea value={body} onChange={(event) => setBody(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Description:
+        <input type="text" value={description} onChange={(event) => setDescription(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Author:
+        <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
- 
-export default Post;
+
+export default PostForm;
